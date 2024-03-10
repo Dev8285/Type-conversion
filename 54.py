5a3916e9a41df96bf056b596c6b75925754888e9
Dev8285 <162889970+Dev8285@users.noreply.github.com>
2024-03-10 18:38:49 +0530

    Add files via upload

---
+++ 54.py
@@ -0,0 +1,43 @@
+# Q. Covert a integer to a floatig-point number.
+
+integer = 45
+print(float(integer))
+
+#  Q. Convert a float to an integer.
+
+float = 34.5
+print(int(float))
+
+# Q. Convert to an integer to a string.
+
+integer = 55
+str = str(55)
+print(str)
+
+# Q. Convert a list to a tupple.
+
+list = [1,2,3,4,5,6,7,8,9]
+print(tuple(list))
+
+# Q. Convert a tuple to a list.
+
+tuple = {1,2,3,4,5,6,7,8,9}
+print(list(tuple))
+
+
+# Q. Convert a decimal number to a binary.
+
+num = 55
+print(bin(num))
+
+
+# Q. Covert a non-zero number to boolean.
+
+num = 45
+boolean_value = bool(num)
+print(boolean_value)
+
+
+
+
+
